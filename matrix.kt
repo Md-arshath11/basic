353a9b70a07ca368be7764daf2b9d1e618821f07
Md-arshath11 <arshath493@gmail.com>
2025-02-21 17:28:27 +0530

    files added

---
+++ matrix.kt
@@ -0,0 +1,22 @@
+fun matrix(){
+
+    for(i in 0..<2){
+        for(j in 0..<2){
+            println("[$i] [$j]")
+        }
+        println()
+    }
+
+
+    for(i in 0..<2){
+        for(j in 0..<3){
+            println("[$i] [$j]")
+        }
+        println()
+    }
+
+}
+
+fun main(){
+    matrix()
+}
