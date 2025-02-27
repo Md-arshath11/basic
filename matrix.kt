a850005bf43fdbf2a786973ba1bc3c318e9c6629
Md-arshath11 <arshath493@gmail.com>
2025-02-27 18:13:38 +0530

    changed

--- matrix.kt
+++ matrix.kt
@@ -1,22 +1,31 @@
-fun matrix(){
+import java.util.Scanner
 
-    for(i in 0..<2){
-        for(j in 0..<2){
+fun matrix() {
+    val scanner = Scanner(System.`in`)
+
+    println("enter the number of rows")
+    val rows = scanner.nextInt()
+
+    println("enter the number of columns")
+    val columns = scanner.nextInt()
+
+    val matrix = Array(rows) { IntArray(columns) }
+    println("matrix element")
+    for (i in 0..<rows) {
+        for (j in 0..<columns) {
             println("[$i] [$j]")
+            matrix[i][j] = scanner.nextInt()
+        }
+    }
+    for (i in 0..<rows) {
+        for (j in 0..<columns) {
+            print("${matrix[i][j]}\t")
         }
         println()
+
+    }
     }
 
-
-    for(i in 0..<2){
-        for(j in 0..<3){
-            println("[$i] [$j]")
-        }
-        println()
+    fun main() {
+        matrix()
     }
-
-}
-
-fun main(){
-    matrix()
-}
